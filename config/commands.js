cccfa0538635041faf3f76d347fd9b6e1e3b0cb2
yaman1337 <yaman.sarabariya@gmail.com>
2023-07-23 12:12:38 +0545

    initiat commit

---
+++ config/commands.js
@@ -0,0 +1,63 @@
+export default {
+  whoami:
+    "Name: Yaman Sarabariya<br />DOB: 23-12-2003<br />Address: Gauriganj 3, Jhapa, Nepal",
+  help: `<table border="1" cellpadding="10">
+    <caption>
+      List of commands
+    </caption>
+
+    <tr>
+      <th>Command</th>
+      <th>Output</th>
+    </tr>
+
+    <tr>
+        <td>whoami</td>
+        <td>General info about me.</td>
+    </tr>
+
+    <tr>
+    <td>projects</td>
+    <td>List of my projects.</td>
+    </tr>
+
+    <tr>
+    <td>social</td>
+    <td>List of my social media handles.</td>
+    </tr>
+
+
+
+  </table>`,
+
+  projects: `<ul style="margin:20px;" >
+  <li>
+      Farmap: <a href="https://github.com">https://github.com</a>
+  </li>
+  <li>
+      Farmap: <a href="https://github.com">https://github.com</a>
+  </li>
+  <li>
+      Farmap: <a href="https://github.com">https://github.com</a>
+  </li>
+  <li>
+      Farmap: <a href="https://github.com">https://github.com</a>
+  </li>
+  <li>
+      Farmap: <a href="https://github.com">https://github.com</a>
+  </li>
+</ul>`,
+
+  social: `<ul style="margin:20px;">
+<li>
+    Facebook: <a href="https://github.com">https://facebook.com</a>
+</li>
+<li>
+    Facebook: <a href="https://github.com">https://facebook.com</a>
+</li>
+<li>
+    Facebook: <a href="https://github.com">https://facebook.com</a>
+</li>
+
+</ul>`,
+};
---
+++ js/main.js
@@ -0,0 +1,102 @@
+import commands from "../config/commands.js";
+
+class Terminal {
+  constructor() {
+    this.inputArray = "";
+    this.total_inputs = "";
+    this.currentInput = "";
+    this.currentInputListener = "";
+    this.shell = document.querySelector("#shell");
+    this.newCommandLine = ` <div class="command">
+    <div class="username">yaman1337:</div>
+    <input type="text" class="command-input" autofocus="true" spellcheck="false">
+    </div>`;
+    this.commands = commands;
+    this.dimensionChanger = document.querySelector("#width-changer");
+    this.terminal = document.querySelector("#terminal");
+    this.terminalMinizer = document.querySelector("#terminal-minimizer");
+    this.dimensionChanger.addEventListener("click", (e) => {
+      this.changeDimensions();
+    });
+    this.terminalMinizer.addEventListener("click", (e) => {
+      this.minimizeTerminal();
+    });
+  }
+
+  init() {
+    this.inputArray = document.querySelectorAll(".command-input");
+    this.total_inputs = this.inputArray.length;
+    this.currentInput = this.inputArray[this.total_inputs - 1];
+
+    this.currentInputListener = this.currentInput.addEventListener(
+      "keypress",
+      async (e) => {
+        if (e.key !== "Enter") {
+          return;
+        }
+
+        // preserve the history of terminal
+        this.currentInput.removeAttribute("autofocus");
+        this.currentInput.setAttribute("value", this.currentInput.value);
+        this.currentInput.setAttribute("readonly", "true");
+
+        // process input
+        await this.processInput(this.currentInput.value);
+
+        // reset the listeners and command inputs
+        this.shell.innerHTML += this.newCommandLine;
+        window.removeEventListener("keypresss", this.currentInputListener);
+        this.init();
+      }
+    );
+  }
+
+  async processInput(input) {
+    switch (input) {
+      case "clear":
+        this.clearTerminal();
+        break;
+      default:
+        let output = this.commands[input];
+        if (!output)
+          this.renderOutput(
+            `<p style="color:red" >Invalid Command: Type help for see list of commands available.</p>`
+          );
+        else this.renderOutput(output);
+    }
+  }
+
+  clearTerminal() {
+    this.shell.innerHTML = "";
+  }
+
+  renderOutput(output) {
+    this.shell.innerHTML += output;
+  }
+
+  changeDimensions() {
+    const documentProps = document.documentElement;
+
+    if (documentProps.clientWidth === this.terminal.clientWidth) {
+      this.terminal.setAttribute("style", "height: 90%; width: 90%");
+    } else {
+      this.terminal.setAttribute("style", "height: 100%; width: 100%");
+    }
+  }
+
+  minimizeTerminal() {
+    const terminalDot = document.querySelector(".terminal-icon > .dot");
+    this.terminal.classList.toggle("close");
+    terminalDot.classList.toggle("open");
+
+    document.querySelector(".terminal-icon").addEventListener("click", (e) => {
+      if (!terminalDot.classList.contains("open")) {
+        this.terminal.classList.toggle("close");
+        terminalDot.classList.toggle("open");
+      }
+    });
+  }
+}
+
+const terminal = new Terminal();
+terminal.init();
